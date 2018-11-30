f1bb85178ca5980ef09f3ee96e85d87ded59a734
Batool Alghamdi <batoolalghamdi@engr2gw-64-187-dhcp.int.colorado.edu>
2018-11-29 18:52:09 -0700

    gg

--- geometry.c
+++ geometry.c
@@ -69,3 +69,15 @@ void coord_2d_midpoint(coord_2d_t* mid, const coord_2d_t* a, const coord_2d_t* b
     mid->y = ((a->y + b->y) / 2.0 );
 
 }
+
+double coord_2d_area_triangle(const coord_2d_t* a, const coord_2d_t* b, const coord_2d_t* c){
+    double base = b->x - a->x;
+    double height = c->y - a->y;
+
+    return 0.5 * base * height;
+}
+
+
+
+
+
